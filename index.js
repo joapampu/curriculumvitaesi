b13561cd9ff15b0faee67cd937660132f8911e97
joapampu <144053900+joapampu@users.noreply.github.com>
2023-11-30 01:33:15 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const colores = ['#FF5733', '#33FF57', '#5733FF', '#FFFF33', '#33FFFF','#000000'];
-
-
-function cambiarColor() {
-    const colorIndex = Math.floor(Math.random() * colores.length);
-    const nuevoColor = colores[colorIndex];
-    document.body.style.backgroundColor = nuevoColor;
-}
